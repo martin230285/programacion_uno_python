4637fe767695ecae3e9b91940ba03fac1410a13e
martin230285 <ubicateenlavida@gmail.com>
2025-03-28 19:44:20 -0300

    Subo Random

---
+++ Random.py
@@ -0,0 +1,7 @@
+import random
+numero_1 = int(input("Elija un numero del 1 al 100: "))
+dado = random.randint(0,100)
+print("Dado: " ,dado)
+numero_2 = int(input("Elija un numero del 1 al 100: "))
+dado = random.randint(0,100)
+print("Dado: " ,dado)
